14c34bbb2e712696373f23edafd00245d809553f
wittyzell <inviolablegenesis@gmail.com>
2024-10-24 00:38:25 +0900

    fix: repo name

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/your-repository-name/' : '',
-  basePath: isProd ? '/your-repository-name' : '',
+  assetPrefix: isProd ? '/hiveonboard/' : '',
+  basePath: isProd ? '/hiveonboard' : '',
   output: 'export'
 };
 
